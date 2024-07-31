95017fa7e809246fb25560ce3affb4fcf80b3283
AndVig <andrea.vigna2@gmail.com>
2024-07-31 09:14:40 +0200

    [UPD] aggiunti Type e technologies all'API

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,7 +11,7 @@ class ProjectController extends Controller
     public function index(){
 
         // $projects= Project::all();
-        $projects= Project::paginate(3);
+        $projects= Project::with('type', 'technologies')->paginate(3);
         return response()->json([
             'status'=>true,
             'results'=> $projects
